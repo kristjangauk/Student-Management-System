d360a3d0ce490e07842311f0c93234d83ee69d47
Kristjan Gauk <kristjangauk@gmail.com>
2021-12-21 19:30:20 +0200

    Initial commit

---
+++ src/main/java/com/example/student_management_spring_thymeleaf/StudentManagementSpringThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.example.student_management_spring_thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagementSpringThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentManagementSpringThymeleafApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/student_management_spring_thymeleaf/StudentManagementSpringThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.student_management_spring_thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementSpringThymeleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
